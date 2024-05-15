9a4698102f4113d51f9e61f38379b47f20d4b843
UCKET <89240280+UCKETX@users.noreply.github.com>
2024-05-15 16:21:09 +0800

    Add files via upload

---
+++ modules/private/secrets/secrets.h
@@ -0,0 +1,49 @@
+#include <string>
+#include <fmt/format.h>
+
+
+class Secrets {
+public:
+	static std::string addSalt(std::string const& original) {
+		return original;
+	}
+	
+	static const unsigned char *getCv4Key() {
+		// PLEASE, GENERATE A RSA KEY, AND PASTE IT BELOW.
+		return (const unsigned char *)"MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALwQjB85vHQM1XaqzmjxVlunVvrnj8p3
+rg1fDDpNnwDK0ZHj7ybE6Qtu0rhcn+bXgIBLYz5hSXjb5hJfW3eNkdcCAwEAAQ==";
+	}
+
+	static std::string get_stripe_key() {
+		if(getenv("DEBUG")) {
+			return "sk_test_";
+		}
+		return "sk_live_";
+	}
+	
+	static std::string get_stripe_webhook_secret() {
+		if(getenv("DEBUG")) {
+			return "whsec_";
+		}
+		return "whsec_";
+	}
+	
+	static std::string get_telegram_bot_token() {
+		return "bot:";
+	}
+	
+	static int64_t get_telegram_chat_id() {
+		return -0;
+	}
+	
+	static std::string get_telegram_webhook_secret() {
+		return "123";
+	}
+	
+	static std::string add_external_bot_salt(std::string const& original) {
+		return original;
+	}
+};
+
+static const unsigned char *_fbtoken_iv=(unsigned char*)"1234567890abcdef";
+static const unsigned char *_fbtoken_key=(unsigned char*)"0123456789abcdefghijklmnopqrstuv";
